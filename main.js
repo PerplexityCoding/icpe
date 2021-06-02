23366a25ce4010ce5c6e2baf593343adf56e6260
PerplexityCoding <ymenard.dev@gmail.com>
2021-06-02 23:37:26 +0200

    feat: improve filters and sytle

--- main.js
+++ main.js
@@ -9,8 +9,23 @@ let data = null;
 let icon = null;
 
 const filters = {};
-const filtersFields = ['num_dep', 'code_naf', 'lib_naf', 'regime', 'lib_regime', 'ippc', 'seveso', 'lib_seveso', 'Seveso', 'famille_ic'];
-const selectedFilters = {};
+const f = (key, label) => ({ key, label});
+const filtersFields = [
+    f('num_dep', 'Département'),
+    f('code_naf', 'Code NAF'),
+    f('lib_naf', 'Libéllé NAF'),
+    f('regime', 'Code Régime'),
+    f('lib_regime', 'Libéllé Regime'),
+    f('lib_seveso', 'Libéllé SEVESO'),
+    f('famille_ic', 'Famille IC'),
+];
+
+const searchTextFields = ['lib_naf', 'famille_ic', 'nom_ets', 'nomcommune'];
+let selectedFilters = {};
+let searchText = null;
+let filtered = false;
+let total = 0;
+let maxTotal = 500;
 
 async function main() {
     await fetchData();
@@ -35,6 +50,9 @@ function createMap() {
         accessToken: 'pk.eyJ1IjoieW1lbmFyZC1kZXYiLCJhIjoiY2twZWc3emZtMGxzYTJubzhyZnp6ZHJveSJ9.ub0yDhdRQ-dWyKBs7KR1wQ'
     }).addTo(map);
 
+    map.on('zoomend', reloadICPE);
+    map.on('moveend', reloadICPE);
+
     return map;
 }
 
@@ -52,17 +70,21 @@ function createIcon() {
 
 async function fetchData() {
     data = await (await fetch('/icpe/assets/data/icpe.geo.json')).json();
+    data.features = data.features.sort(() => Math.random() - 0.5);
 }
 
 function createFilers() {
-    for (const field of filtersFields) {
+    for (const { key, label } of filtersFields) {
         const props = {};
         for (const feature of data.features) {
-            props[feature.properties[field]] = true;
+            props[feature.properties[key]] = true;
         }
-        filters[field] = Object.keys(props).sort();
-        if (filters[field][0] !== '') {
-            filters[field].unshift('');
+        filters[key] = {
+            label,
+            items: Object.keys(props).sort()
+        };
+        if (filters[key].items[0] !== '') {
+            filters[key].items.unshift('');
         }
     }
 }
@@ -70,24 +92,36 @@ function createFilers() {
 function displayFilters() {
     let filterHtml = '';
 
-    for (const [field, filter] of Object.entries(filters)) {
+    filterHtml += `
+        <fieldset class="search-bar">
+            <label>Recherche </label><input type='text' class='search' />
+        </fieldset>
+        <p class="filters-title">
+          Filtres
+        </p>
+    `;
+
+    for (const [field, {label, items}] of Object.entries(filters)) {
         let options = '';
-        for (const option of filter) {
+        for (const option of items) {
             options += `<option>${option}</option>`
         }
 
         filterHtml += `
             <fieldset>
-                <label>${field}</label>
+                <label>${label}</label>
                 <select name="${field}">${options}</select>
-            </fieldset>        
+            </fieldset>
         `
     }
 
     filterHtml += `
-        <button class='refresh'>
-            Refresh
-        </button>      
+        <button class='reset'>
+            Réinitialiser
+        </button>    
+
+        <div class="pinNumber">
+        </div>
     `;
 
     document.querySelector('.filters').innerHTML = filterHtml;
@@ -98,12 +132,32 @@ function displayFilters() {
             reloadICPE();
         });
     });
-    document.querySelector('.refresh').addEventListener('click', () => {
+
+    const search = document.querySelector('.search');
+    search.addEventListener('focusout', () => {
+        const value = search.value.trim();
+        searchText = value;
+        reloadICPE();
+    });
+    search.addEventListener('keypress', (e) => {
+        if (e.keyCode === 13) {
+            const value = search.value.trim();
+            searchText = value;
+            reloadICPE();
+        }
+    });
+
+    document.querySelector('.reset').addEventListener('click', () => {
+        selectedFilters = {};
+        maxTotal = 500;
         reloadICPE();
     });
 }
 
 function reloadICPE() {
+    filtered = false;
+    total = 0;
+
     if (poiLayer) {
         poiLayer.remove();
     }
@@ -116,10 +170,26 @@ function reloadICPE() {
         },
         filter: function (feature, layer) {
             const properties = feature.properties;
+
             const entries = Object.entries(selectedFilters);
 
-            if (entries.length == 0) {
+            if (total >= maxTotal) {
+                filtered = true;
                 return false;
+            }
+
+            if (searchText && searchText !== '' && searchText.length >= 3) {
+                let match = false;
+                for (const searchTextField of searchTextFields) {
+                    if (properties[searchTextField].match(new RegExp(searchText, 'ig'))) {
+                        match = true;
+                        break;
+                    }
+                }
+
+                if (! match) {
+                    return false;
+                }
             }
 
             const coord = feature.geometry.coordinates;
@@ -130,10 +200,12 @@ function reloadICPE() {
             }
 
             for (const [field, selectedFilterValue] of entries) {
-                if (properties[field] !== selectedFilterValue) {
+                if (selectedFilterValue !== '' && properties[field] !== selectedFilterValue) {
                     return false;
                 }
             }
+
+            total++;
             return true;
         },
         onEachFeature: function (feature, layer) {
@@ -141,7 +213,19 @@ function reloadICPE() {
 
             if (feature.properties) {
                 if (feature.properties.nom_ets) {
-                    popupContent += feature.properties.nom_ets;
+                    popupContent += `<b>${feature.properties.nom_ets}</b> <br />`;
+                }
+                if (feature.properties.lib_naf) {
+                    popupContent += `${feature.properties.lib_naf} <br />`;
+                }
+                if (feature.properties.famille_ic) {
+                    popupContent += `${feature.properties.famille_ic}  <br />`;
+                }
+                if (feature.properties.lib_seveso) {
+                    popupContent += `${feature.properties.lib_seveso}  <br />`;
+                }
+                if (feature.properties.nomcommune) {
+                    popupContent += `${feature.properties.nomcommune}  <br />`;
                 }
                 if (feature.properties.url_fiche) {
                     popupContent += `<br /><a target="_blank" href=${feature.properties.url_fiche}> voir fiche </a>`
@@ -151,6 +235,19 @@ function reloadICPE() {
             layer.bindPopup(popupContent);
         }
     }).addTo(map);
+
+    const totalPins = poiLayer.getLayers().length;
+
+    document.querySelector('.pinNumber').innerHTML =
+        `
+            <p class="search-result">
+                Résultat de recherche: <b>${totalPins}</b> entrée(s)
+                ${ filtered ? '<br /><br /><b>(Par soucis de performance, tous les élements ne sont pas afficher. Zoomer pour voir plus)</b> <br />': '' }
+                <button class="addMaxTotal"> +500 </button>                
+            </p>
+        `
+    ;
+    document.querySelector('.addMaxTotal').addEventListener('click', () => maxTotal += 500);
 }
 
 main();
